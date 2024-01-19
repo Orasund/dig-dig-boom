9bd5f6f11fa11386336d00fd330192bc8c9aec45
Lucas Payr <orasund@gmail.com>
2024-01-19 17:32:23 +0100

    fix starting room

--- src/Main.elm
+++ src/Main.elm
@@ -78,9 +78,8 @@ init : flag -> ( Model, Cmd Msg )
 init _ =
     let
         room =
-            ( 2, -11 )
+            ( 0, 0 )
 
-        --( 2, -8 )
         initialPlayerPos =
             ( 2, 4 )
 
